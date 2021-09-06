358455b11d2dfdf77696383c390210c3a8346e9c
root <wingedpower@gmail.com>
2021-09-06 14:51:54 -0700

    Working checkpoint.

--- top.v
+++ top.v
@@ -6,15 +6,18 @@ module double_edge_detect(clk,reset,in,out);
    output out;
    reg last_in;
    reg out;
-   reg [3:0] state_current, state_next;
+   reg [2:0] state_current, state_next;
    wire clk,in,reset;
    // Different triggered states
-   reg [3:0]
-      not_triggered = 3'b000,
-      pos_triggered = 3'b001,
-      pos_post      = 3'b011,
-      neg_triggered = 3'b101,
-      neg_post      = 3'b111;
+   localparam [2:0]
+      not_triggered_neg = 3'b000,
+      pos_triggered     = 3'b001,
+      pos_one_clk       = 3'b010,
+      pos_post          = 3'b011,
+      not_triggered_pos = 3'b100,
+      neg_triggered     = 3'b101,
+      neg_one_clk       = 3'b110,
+      neg_post          = 3'b111;
 
     // when resetting, set the state back to not triggered, and also set
     // output to 0
@@ -23,101 +26,64 @@ module double_edge_detect(clk,reset,in,out);
        begin
          out = 0;
          last_in = 0;
-         state_current  = not_triggered;
+         state_current  = not_triggered_neg;
        end
-   
-   always @(posedge in)
-   begin
-     if ( last_in == 0 )
-     begin
-       state_current = pos_triggered;
-       last_in = 1;
-     end
-   end
-
-   always @(negedge in)
-   begin
-     if ( last_in == 1 )
-     begin
-       state_current = neg_triggered;
-       last_in = 0;
-     end
-   end
 
    // Okay, now let's have things change based on the positive edge of the
    // clock
+   always @(negedge clk)
+   begin
+     state_current = state_next;
+   end
+
    always @(posedge clk)
    begin
-      state_next = state_current;
-
-      case( state_current )
-        not_triggered:
+     state_next = state_current;
+      case (state_current)
+        not_triggered_neg:
         begin
-          if ( in == 0 )
-          begin
-            if ( last_in > 0 )
-            begin
-              // in = low, previously was high
-              state_next = neg_triggered;
-              last_in = in;
-            end
-            else
-            begin
-              // in = low and prev was low
-              last_in = in;
-            end
-          end
-
-          if ( in == 1 )
-          begin
-            if ( last_in < 1)
-            begin
-              // in = high, prev was low
-              state_next = pos_triggered;
-              last_in = in;
-            end
-            else
-            begin
-              // in = high, last was high
-              last_in = in;
-            end
-          end
-        end
-
-        neg_triggered:
-        begin
-          out = 1;
-          last_in = in;
-          state_next = neg_post;
-        end
-
-        neg_post:
-        begin
-          out = 0;
-          last_in = in;
-          state_next = not_triggered;
+          state_next = state_next + in;
         end
 
         pos_triggered:
         begin
           out = 1;
-          last_in = in;
-          state_next = pos_post;
+          state_next = state_next + 1;
+        end
+
+        pos_one_clk:
+        begin
+          out = 0;
+          state_next = state_next + 1;
         end
 
         pos_post:
         begin
-          out = 0;
-          last_in = in;
-          state_next = not_triggered;
+          state_next = state_next + 1;
         end
 
-/*
-        default:
+        not_triggered_pos:
         begin
-          state_next = not_triggered;
+          state_next = state_next + ( 1 - in );
         end
-*/
+
+        neg_triggered:
+        begin
+          out = 1;
+          state_next = state_next + 1;
+        end
+
+        neg_one_clk:
+        begin
+          out = 0;
+          state_next = state_next + 1;
+        end
+
+        neg_post:
+        begin
+          state_next = state_next + 1;
+        end
+
       endcase
   end
 endmodule
--- top_tb.v
+++ top_tb.v
@@ -14,26 +14,24 @@ module sim;
      clk = 1'b0;
      in  = 1'b0;
      sigtog = 5'b0;
-     #1 reset = 1'b0;
-     #3 reset = 1'b1;
+     #2 reset = 1'b0;
+     #6 reset = 1'b1;
      #2 reset = 1'b0;
   end
   always
-     #5 clk = ~clk;
+     #3 clk = ~clk;
 
   always @(posedge clk)
   begin
-    in = 0;
     if ( tally > 1000 )
       $finish;
     else
       tally <= tally + 1;
 
-    if (sigtog == 4 )
-      in = 1;
-
-    if (sigtog == 1 )
-      in = 0;
+    if (sigtog == 7 )
+      in = ~in;
+    if (sigtog == 8 )
+      in = ~in;
 
     sigtog <= sigtog + 1;
   end
